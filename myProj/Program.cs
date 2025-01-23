d8b18db16eb97df00c6363937bdc9e2f20ee6c5c
bhaneshvar007 <bhaneshvarkshirsagar0207@gmail.com>
2025-01-23 15:22:42 +0530

    d

--- myProj/Program.cs
+++ myProj/Program.cs
@@ -96,3 +96,73 @@
 
     }
 }
+
+
+
+
+// // class Program
+// {
+//     public static void Main(string[] args)
+//     {
+//         // Console.Write("Enter any number ");
+//         // int num = Convert.ToInt32(Console.ReadLine());
+
+//         // int fact = 1;
+//         // for (int i = 2; i <= num; i++)
+//         // {
+//         //     fact *= i;
+//         // }
+//         // Console.WriteLine("Result is : " + fact);
+
+//         // Console.WriteLine("for Palindrome number ");
+//         // Console.Write("Enter any number : ");
+//         // int n = Convert.ToInt32(Console.ReadLine());
+
+//         // int temp = n;
+
+//         // int rev  = 0;
+
+//         // while(n!=0){
+//         //     int rem = n %10;
+//         //     rev = rev * 10 + rem;
+//         //     n = n / 10;
+//         // }
+
+//         // if(rev == temp) Console.WriteLine("True");
+//         // else Console.WriteLine("False");
+
+
+
+//         // Console.Write("Armstrong number : ");
+//         // int n1 = Convert.ToInt32(Console.ReadLine());
+
+//         // int len = 0;
+//         // int temp1 = n1;
+
+//         // while (n1 != 0)
+//         // {
+//         //     n1 = n1 / 10;
+//         //     len++;
+//         // }
+//         // n1 = temp1;
+//         // int res = 0;
+
+//         // while (n1 != 0)
+//         // {
+//         //     int arm = n1 % 10;
+//         //     int pow = 1;
+
+//         //     for (int i = 0; i < len; i++)
+//         //     {
+//         //         pow *= arm;
+//         //     }
+
+//         //     res += pow;
+//         //     n1 = n1 / 10;
+//         // }
+
+//         // if (res == temp1) Console.WriteLine("Armstrong number ");
+//         // else Console.WriteLine("Not a Armstrong number ");
+
+//     }
+// }
